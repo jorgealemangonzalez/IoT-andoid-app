d4a9baa28934c314aea2c3b31be377cf45413c97
jorge <jorge.alemangonzalez@gmail.com>
2018-05-16 10:17:42 +0200

    Manager first release

--- app/src/main/java/com/jorge/arnau/iot/RFIDManager.java
+++ app/src/main/java/com/jorge/arnau/iot/RFIDManager.java
@@ -1,25 +1,92 @@
 package com.jorge.arnau.iot;
 
+import android.util.Log;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.StringWriter;
+import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class RFIDManager {
     private static RFIDManager theInstance = null;
-    private String urlConnexion = "http://10.0.2.2:3161";
-    private String urlDevices = urlConnexion+"/devices";
+    private String urlConnexion = "http://10.0.2.2:3161";   //TODO PUEDE QUE HAYA QUE CAMBIARLO CON UN MOVIL REAL
+    private URL urlDevices = new URL(urlConnexion+"/devices/simulator/inventory");
     private Document devicesDoc = null;
 
-    private RFIDManager(){
+    private RFIDManager() throws MalformedURLException {
 
     }
 
+    private String XMLDocumentAsString(Document doc){
+        //XML as string
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = null;
+        try {
+            transformer = tf.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter writer = new StringWriter();
+        try {
+            transformer.transform(new DOMSource(doc), new StreamResult(writer));
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        String output = writer.getBuffer().toString(); //.replaceAll("\n|\r", "")
+
+        // end XML as string
+        return output;
+    }
+
     public static RFIDManager getInstance(){
-        if(RFIDManager.theInstance == null)
-            RFIDManager.theInstance = new RFIDManager();
+        if(RFIDManager.theInstance == null) {
+            try {
+                RFIDManager.theInstance = new RFIDManager();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
         return RFIDManager.theInstance;
     }
 
     public void readDevices(){
-        FetchHttpRequest req = new FetchHttpRequest();
-        Document doc = req.execute(this.urlDevices);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        try {
+            Document doc = factory.newDocumentBuilder().parse(urlDevices.openStream());
+            this.devicesDoc = doc;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> getConnectedDevices(){
+        NodeList devices =  this.devicesDoc.getElementsByTagName("item");
+        List<String> resutIds = new ArrayList<>();
+        for( int i = 0 ; i < devices.getLength(); ++i){
+            Element device = (Element) devices.item(i);
+            String id = ((Element)device.getElementsByTagName("epc").item(0)).getTextContent();
+            Log.i("GET CONNECTED DEVICES", id);
+            resutIds.add(id);
+        }
+        return resutIds;
     }
 }
